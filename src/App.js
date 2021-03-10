0c0def5dcb87c3a39291532ff5547c65a1a19382
hajar nasr <hajar-nasr@hotmail.com>
2021-03-10 15:58:14 +0200

    feat: create formatText

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import './App.css';
+import "./App.css";
 import React from "react";
+import { formatText } from "./helpers";
 
 function App() {
   const [textInput, setTextInput] = React.useState(`This is
 a badly formatted file. This line is pretty long! It's way more than 80 characters! I feel a line wrap coming on!
 
 This      is a second paragraph with extraneous whitespace.`);
-  const [textOutput, setTextOutput] = React.useState('');
+  const [textOutput, setTextOutput] = React.useState("");
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setTextInput(event.target.value);
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     transformText(textInput);
   };
 
-  const transformText = input => {
-    let output = input;
-    /*
-    your work goes here!
-    */
+  const transformText = (input) => {
+    let output = formatText(input);
     setTextOutput(output);
-  }
-  
+  };
+
   return (
     <div className="App">
       <header>
@@ -32,13 +30,16 @@ This      is a second paragraph with extraneous whitespace.`);
       </header>
       <form onSubmit={handleSubmit}>
         <label>
-          <textarea onChange={handleChange} value={textInput}/>
+          <textarea
+            onChange={handleChange}
+            value={textInput}
+            className="input-textarea"
+          />
         </label>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Submit" />
       </form>
-      <div id="result">
-        {textOutput}
-      </div>
+      <div id="result">{textOutput}</div>
+      {textOutput && <button onClick={() => setTextOutput("")}>Clear</button>}
     </div>
   );
 }
